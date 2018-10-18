0b0fb12871016f513fcbc608b334594df38c565b
Rose Parker <parker.rose@ymail.com>
2018-10-18 01:36:52 +0000

    app.get route to /blocks that creates an array of blocks and serializes it back to the client using res.json function

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/blocks', function(req, res) {
+    var blocks = ['Fixed', 'Movable', 'Rotating'];
+    res.json(blocks);
+});
+
 app.listen(process.env.PORT, process.env.IP, 8080, function() {
     console.log('Listening on port 8080');
 });
