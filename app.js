cb1e24bff43429a1f879131bcb50be9ed877798b
Rose Parker <parker.rose@ymail.com>
2018-10-18 00:00:26 +0000

    rewrote code to use the express static middleware instead

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static('public'));
 
 app.listen(process.env.PORT, process.env.IP, 8080, function() {
     console.log('Listening on port 8080');
