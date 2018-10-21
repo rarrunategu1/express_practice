e7ac76955322623ba7ce74a85beddc6ecb1fbbde
Rose Parker <parker.rose@ymail.com>
2018-10-21 22:00:04 +0000

    chained some function calls

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('./logger');
 var bodyParser = require('body-parser');
 var parseUrlencoded = bodyParser.urlencoded({extended: false }); //fores use of the natie querystring Node Library
 
+
+
 app.use(logger);
 
 app.use('/', express.static('public'));
@@ -14,39 +16,24 @@ app.use('/', express.static('public'));
     'Movable': 'Capable of being moved',
     'Rotating': 'Moving in a circle around its center'};
     
-//create delete route that calls delete function
-app.delete('/blocks/:name', function(req, res) {
-    delete blocks[req.blockName];  //removes entry from blocks object above
-    res.sendStatus(200);  //sets response body to ok
-    
-});
+var locations = {
+    'Fixed': 'First floor', 'Movable': 'Second floor', 'Rotating': 'Penthouse'
+};
 
-app.post('/blocks', parseUrlencoded, function(req, res) {
+
+//using chain function below to chain some function calls
+app.route('/blocks') //returns route object that handles all requests to the /blocks path
+.get(function(req, res) {
+    res.json(Object.keys(blocks));
+})
+.post(parseUrlencoded, function(req, res) {
     var newBlock = req.body;
     blocks[newBlock.name] = newBlock.description;
     
     res.status(201).json(newBlock.name);
 });
-
-    
-app.get('/blocks', function(req, res) {
-    res.json(Object.keys(blocks));
-});
-    
-var locations = {
-    'Fixed': 'First floor', 'Movable': 'Second floor', 'Rotating': 'Penthouse'
-};
-
-app.param('name', function(req, res, next) {
-    var name = req.params.name;
-    var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
-    
-    req.blockName = block;
-    
-    next();
-});
-
-app.get('/blocks/:name', function(req, res) {
+app.route('/blocks/:name')
+.get(function(req, res) {
    var description = blocks[req.blockName];
    if (!description) {
        res.status(404).json('No description found for ' + req.params.name); //sends a return if no block is found with the param entered
@@ -55,8 +42,21 @@ app.get('/blocks/:name', function(req, res) {
        
        res.json(description);  //will also set status code to 200 success
 }
+})
+//create delete route that calls delete function
+.delete(function(req, res) {
+    delete blocks[req.blockName];  //removes entry from blocks object above
+    res.sendStatus(200);  //sets response body to ok
     
+});
+
+app.param('name', function(req, res, next) {
+    var name = req.params.name;
+    var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
     
+    req.blockName = block;
+    
+    next();
 });
 
 app.get('/locations/:name', function(req, res) {
