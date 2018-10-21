6477727ebd0a4d2a5a5945e8f6048eadbb0bfaf8
Rose Parker <parker.rose@ymail.com>
2018-10-21 21:15:14 +0000

    added delete link to each block item and added a click event on the delete links

--- public/client.js
+++ public/client.js
@@ -6,10 +6,27 @@ $(function(){
         for(var i in blocks){
             block = blocks[i];
             content = '<a href="/blocks/' +block+'">'+block+'</a>'; //link to each blocks description
+            //adds delete link with image to each block item
+            'a href="#" data-block="'+block+'"><img src="del.jpg"></a>';
             list.push($('<li>', { html: content }));
         }
         $('.block-list').append(list);
     }
+    
+    //click events on the delete links
+    $('.block-list').on('click', 'a[data-block]', function(event){
+        if (!confirm('Are you sure?')) {  //clicking cancel will stop the delete eventhandler
+            return false;
+        }
+        
+        var target = $(event.currentTarget); //gets link that triggered the click event
+        
+        $.ajax({
+            type: 'DELETE', url: '/blocks/' + target.data('block')
+            }).done(function() {
+                target.parents('li').remove();
+            });
+    });
     
     $('form').on('submit', function(event) {
         event.preventDefault();
