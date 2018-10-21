1dd57e9ab4b21357df75a726dfcce332d5b20fad
Rose Parker <parker.rose@ymail.com>
2018-10-21 18:52:45 +0000

    added form on submit function for new blok data and ajax post

--- public/client.js
+++ public/client.js
@@ -4,8 +4,23 @@ $(function(){
     function appendToList(blocks) {
         var list = [];
         for(var i in blocks){
-            list.push($('<li>', { text: blocks[i] }));
+            block = blocks[i];
+            content = '<a href="/blocks/' +block+'">'+block+'</a>'; //link to each blocks description
+            list.push($('<li>', { html: content }));
         }
         $('.block-list').append(list);
     }
+    
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        var form = $(this);
+        var blockData = form.serialize(); //transforms form data to url encoded notation so that express app can parse back to javascript
+        
+        $.ajax({
+            type: 'POST', url: '/blocks', data: blockData
+        }).done(function(blockName){
+            appendToList([blockName]);
+            form.trigger('reset'); //cleans up form text input fields
+        });
+    });
 });
